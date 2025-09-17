101ac5d377062ea2b7b6d12e20184fa6b3537c07
Kivira-Vehan <pliyanage730@gmail.com>
2025-09-17 20:51:03 +0530

    Int-Task-1(update)

--- Session-0/main.py
+++ Session-0/main.py
@@ -5,11 +5,11 @@ def print_christmas_tree(x):
 
 ## x0 = "apple", y0 = "adcsjncjsppaxjjnaxle" --> True if all characters of x0 are in y0 in order, y0 = "bsdpple" --> False, y0 = "paple" --> False
 def is_subsequence(x0, y0):
-        i = 0  # pointer for x0
-        for letter in y0:
-            if i < len(x0) and x0[i] == letter:
-                i += 1
-        return i == len(x0)
+    i = 0  # pointer for x0
+    for letter in y0:
+        if i < len(x0) and x0[i] == letter:
+            i += 1
+    return i == len(x0)
 
 
 x= int(input("Enter Height: "))
